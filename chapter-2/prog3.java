8b78c0da4041cbd560aec0fa6dd3b7147f93af46
cvashanku <cvashankar18@gmail.com>
2022-11-20 12:10:31 +0530

    recursion

---
+++ chapter-2/prog3.java
@@ -0,0 +1,25 @@
+//Mathematical Interpretation of Recursion
+//sum of n natural numbers
+import java.util.*;
+
+class demo
+{
+  static int sum(int n)
+  {
+    if(n==1)
+      return 1;
+    else
+      return n+sum(n-1);
+  }
+}
+
+class test
+{
+  public static void main(String args[])
+  {
+    Scanner obj=new Scanner(System.in);
+    System.out.println("enter n value");
+    int n=obj.nextInt();
+    System.out.println(demo.sum(n));
+  }
+}
