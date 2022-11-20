62ae1310a534bb4d7c5f30cfa9ea7431f51c4221
cvashanku <cvashankar18@gmail.com>
2022-11-20 11:32:34 +0530

    Data Structures and Algorithms with Java

---
+++ chapter-2/prog1.java
@@ -0,0 +1,16 @@
+class demo
+{
+  static void m()
+  {
+    System.out.println("good morning");
+    m();
+  }
+}
+
+class test
+{
+  public static void main(String args[])
+   {
+     demo.m();
+   }
+}
