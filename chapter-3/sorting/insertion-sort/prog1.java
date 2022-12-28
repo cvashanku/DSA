e4791c554355e4d91924619dc069605694c5a89a
cvashanku <cvashankar18@gmail.com>
2022-12-29 00:17:26 +0530

    sorting

---
+++ chapter-3/sorting/insertion-sort/prog1.java
@@ -0,0 +1,38 @@
+//program to demonstrate insertion sort in ascending order
+
+import java.util.*;
+
+class demo
+{
+  static void insertionSortAsc(int[] a)
+    {
+      int i,j,temp,n=a.length;
+       for(i=1;i<n;i++)
+        {
+          temp=a[i];
+          j=i-1;
+          while(j>=0 && a[j]>temp)
+           {
+             a[j+1]=a[j];
+             j--;
+           }
+           a[j+1]=temp;
+        }
+    }
+}
+
+class test
+{
+  public static void main(String args[])
+    {
+      Random r=new Random();
+      int[] a=new int[10];
+      for(int i=0;i<a.length;i++)
+        {
+          a[i]=r.nextInt(100);
+        }
+       System.out.println("before sorting===>"+Arrays.toString(a));
+       demo.insertionSortAsc(a);
+       System.out.println("After sorting===>"+Arrays.toString(a));
+    }
+}
