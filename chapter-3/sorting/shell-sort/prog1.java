37fd4162366d830f9d81d6b4b02be6ed2420cacc
cvashanku <cvashankar18@gmail.com>
2023-01-03 12:23:38 +0530

    shell sort

---
+++ chapter-3/sorting/shell-sort/prog1.java
@@ -0,0 +1,44 @@
+//program to demonstrate shell sort algorithm in Ascending order
+
+import java.util.*;
+
+class Demo
+{
+   static void shellSortAsc(int[] a,int n)
+     {
+        int gap,i,j,temp;
+        for(gap=n/2;gap>=1;gap=gap/2)
+          {
+             for(j=gap;j<n;j++)
+               {
+                 for(i=j-gap;i>=0;i=i-gap)
+                   {
+                      if(a[i+gap]>a[i])
+                        break;
+                      else
+                        {
+                           temp=a[i+gap];
+                           a[i+gap]=a[i];
+                           a[i]=temp;
+                        }
+                   }
+               }
+          }
+     }
+}
+
+class Test
+{
+  public static void main(String[] args)
+    {
+       Random r=new Random();
+       int a[]=new int[10];
+       for(int i=0;i<a.length;i++)
+         {
+           a[i]=r.nextInt(100);
+         }
+       System.out.println("Before sorting===>"+Arrays.toString(a));
+       Demo.shellSortAsc(a,a.length);
+       System.out.println("After sorting===>"+Arrays.toString(a));
+    }
+}
