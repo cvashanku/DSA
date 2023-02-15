7927ac6968cfd630683d4cb49e8b2ceda5ac536f
cvashanku <cvashankar18@gmail.com>
2023-02-15 18:09:13 +0530

    single linked list

--- linkedlist/SLL/prog1.java
+++ linkedlist/SLL/prog1.java
@@ -93,6 +93,122 @@ class SLL
      }
   }
 
+  void sortedInsertAsc(int data)
+  {
+    Node newNode=new Node(data);
+    Node currNode=head;
+    if(currNode==null || currNode.data>data)
+    {
+       newNode.next=head;
+       head=newNode;
+       return;
+    }
+    while(currNode.next!=null && currNode.next.data<data)
+        currNode=currNode.next;
+    newNode.next=currNode.next;
+    currNode.next=newNode;
+  }
+
+  void sortedInsertDesc(int data)
+  {
+    Node newNode=new Node(data);
+    Node currNode=head;
+    if(currNode==null || currNode.data<data)
+    {
+       newNode.next=head;
+       head=newNode;
+       return;
+    }
+    while(currNode.next!=null && currNode.next.data>data)
+        currNode=currNode.next;
+    newNode.next=currNode.next;
+    currNode.next=newNode;
+  }
+
+  int getSize()
+  {
+    return this.size;
+  }
+
+  boolean search(int data)
+  {
+    Node currNode=head;
+    while(currNode!=null)
+    {
+      if(currNode.data==data)
+        return true;
+      currNode=currNode.next;
+    }
+    return false;
+  }
+
+  void deleteFirst()
+  {
+    if(head==null)
+    {
+      System.out.println("SLL is empty");
+      return;
+    }
+    size--;
+    head=head.next;
+  }
+
+  void deleteLast()
+  {
+    if(head==null)
+    {
+      System.out.println("SLL is empty");
+      return;
+    }
+    if(head.next==null)
+    {
+      head=null;
+      size--;
+      return;
+    }
+    size--;
+    Node temp1=head,temp2=head.next;
+    while(temp2.next!=null)
+    {
+      temp2=temp2.next;
+      temp1=temp1.next;
+    }
+    temp1.next=null;
+  }
+
+  void deletePos(int pos)
+  {
+    Node temp=head;
+    int i=0;
+    if(temp==null)
+    {
+      System.out.println("SLL is empty");
+      return;
+    }
+    if(pos==0)
+    {
+      head=head.next;
+      size--;
+      return;
+    }
+    if(pos<0 || pos>size-1)
+    {
+      System.out.println("out of range");
+      return;
+    }
+    while(temp.next!=null && i<pos)
+    {
+      if(i==pos-1)
+      {
+        temp.next=temp.next.next;
+        size--;
+        return;
+      }
+      i++;
+      temp=temp.next;
+    }
+  }
+
 }
 
 class Test
@@ -102,14 +218,14 @@ class Test
      SLL list=new SLL();
      list.addLast(111);
      list.addLast(222);
-     list.addFirst(18);
      list.addLast(333);
      list.addLast(444);
      list.addLast(555);
+     list.addLast(666);
+     list.addLast(777);
      list.traverse();
-     list.addPos(7,6);
-     list.addPos(45,3);
+     list.deletePos(7);
      list.traverse();
-   
+     System.out.println(list.search(66));  
    }
 }
